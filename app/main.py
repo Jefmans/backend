2d3cfb02ffe3f94b058eefc0f9c3b09145527025
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-04-22 17:11:36 +0200

    added health route in main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 # from .routes import router
-from app.routes import search
+from app.routes import search, health
 
 app = FastAPI()
 # app.include_router(router)
 
 
 app.include_router(search.router, prefix="/api")
+app.include_router(health.router, prefix="/api")
