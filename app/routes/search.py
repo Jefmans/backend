418da5f8ff347e44afbb8d9dcf856138602e1fde
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-04-22 18:00:47 +0200

    added mutiple searches to test

--- app/routes/search.py
+++ app/routes/search.py
@@ -7,28 +7,28 @@ router = APIRouter()
 
 es = Elasticsearch("http://elasticsearch:9200")
 
-# @router.get("/search")
-# def search(q: str = Query(..., description="Search term")):
-#     response = es.search(
-#         index="your-index-name",
-#         query={"match": {"content": q}}
-#     )
-#     return response["hits"]
+@router.get("/search/1")
+def search(q: str = Query(..., description="Search term")):
+    response = es.search(
+        index="my-index",
+        query={"match": {"content": q}}
+    )
+    return response["hits"]
 
 
-# @router.get("/search")
-# def search_test_data():
-#     response = es.search(
-#         index="my-index",
-#         query={"match_all": {}}
-#     )
-#     hits = response["hits"]["hits"]
-#     return [{"id": hit["_id"], "data": hit["_source"]} for hit in hits]
+@router.get("/search/2")
+def search_test_data():
+    response = es.search(
+        index="my-index",
+        query={"match_all": {}}
+    )
+    hits = response["hits"]["hits"]
+    return [{"id": hit["_id"], "data": hit["_source"]} for hit in hits]
 
 
 
 
-@router.get("/search")
+@router.get("/search/3")
 def search_test_data():
     response = es.search(
         index="my-index",
