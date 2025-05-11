a88f83be8fc1ab9eb783624e1be9d1194bd41fdb
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-05-11 18:28:28 +0200

    docs url

--- app/main.py
+++ app/main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI
 # from .routes import router
 from app.routes import search, health
 
-app = FastAPI()
-# app.include_router(router)
+# app = FastAPI()
+# # app.include_router(router)
+
+app = FastAPI(
+    docs_url="/docs",  # Make sure the documentation URL is correct
+    redoc_url="/redoc",  # Make sure ReDoc URL is correct if you're using it
+)
 
 
 app.include_router(search.router, prefix="/api")
